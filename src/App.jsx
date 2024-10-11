4db4948cc1052f352738fa5515e8b918a7bf1929
Aceee03 <116922402+Aceee03@users.noreply.github.com>
2024-10-11 16:37:01 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HeadlineCards from './components/HeadlineCards'
+import Food from './components/Food'
+
+
+
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <Navbar />
+        <Hero />
+        <HeadlineCards />
+        <Food />
+      </div>
+    </>
+  )
+}
+
+export default App
